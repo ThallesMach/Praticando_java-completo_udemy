ceed22444a75268d527b6e816dc0d1ea18658720
Thalles Machado <35805076+ThallesMach@users.noreply.github.com>
2021-05-12 16:32:03 -0300

    Add Arquivo via UpLoad Java Programacao
    Add Arquivo via UpLoad Projeto Java Programacao

---
+++ course_javaprogramacao/src/course_javaprogramacao/Main.java
@@ -0,0 +1,20 @@
+package course_javaprogramacao;
+
+import java.util.Locale;
+
+public class Main {
+	
+	public static void main(String[] args) {
+						
+		System.out.println("------------");
+		System.out.println(" Ola Mundo");
+		System.out.println("------------");
+
+		
+	}
+
+}
+
+
+
+
