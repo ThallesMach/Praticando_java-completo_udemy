7c7deac078c90cf1fa0a8446245e482b2ea8624a
Thalles Machado <35805076+ThallesMach@users.noreply.github.com>
2021-05-12 18:00:34 -0300

    Add Projet inicial teste

---
+++ Course_Java/src/main_Teste.java
@@ -0,0 +1,12 @@
+
+public class main_Teste {
+
+public static void main(String[] args) {
+				
+		System.out.println("------------");
+		System.out.println(" Ola Mundo");
+		System.out.println("------------");
+		
+	}
+
+}
